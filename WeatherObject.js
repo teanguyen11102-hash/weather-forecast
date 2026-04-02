a13c0ad78ebd7aff2f54191cf970b50ab12c9d95
Tran Nguyen <teanguyen11102@gmail.com>
2026-04-02 22:47:59 +0700

    Weather Object

---
+++ WeatherObject.js
@@ -0,0 +1,9 @@
+let currentWeather = {
+    temperature: 35,
+    conditions: "sunny",
+    sunriseTime: "7am",
+    sunsetTime: "5pm"
+}
+console.log(currentWeather.temperature);
+
+console.log(`It's ${currentWeather.temperature} degree F and ${currentWeather.conditions}`)
