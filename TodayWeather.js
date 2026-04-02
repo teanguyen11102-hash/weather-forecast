6fd77d89c32d6036403ed26c98af104718d417a8
Tran Nguyen <teanguyen11102@gmail.com>
2026-04-02 22:43:08 +0700

    Today's Weather

---
+++ TodayWeather.js
@@ -0,0 +1,9 @@
+let temperature = 35;
+let conditions = "sunny"
+let sunriseTime= "7am"
+let sunsetTime = "5pm"
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+console.log(` It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`)
